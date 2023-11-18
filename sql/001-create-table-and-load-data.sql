676ca8cb7b7c3ef60bb6e4625fdcd919b30641e8
tomoya0844 <dpon3285@yahoo.co.jp>
2023-11-18 22:31:04 +0900

    追加

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,11 +3,14 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE users (
   id int unsigned AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
-  occupation VARCHAR(100) NOT NULL,
+  occupation VARCHAR(100) NOT NULL UNIQUE,
   PRIMARY KEY(id)
 );
 
 INSERT INTO users (name, occupation) VALUES ("田中", "医者");
 INSERT INTO users (name, occupation) VALUES ("小松", "小学校教諭");
-INSERT INTO users (name, occupation) VALUES ("小松", "SE");
-INSERT INTO users (name, occupation) VALUES ("小松", "農家");
+INSERT INTO users (name, occupation) VALUES ("佐藤", "SE");
+INSERT INTO users (name, occupation) VALUES ("木村", "農家");
+INSERT INTO users (name, occupation) VALUES ("平野", "ダンサー");
+INSERT INTO users (name, occupation) VALUES ("高橋", "歌手");
+INSERT INTO users (name, occupation) VALUES ("久保", "YouTuber");
