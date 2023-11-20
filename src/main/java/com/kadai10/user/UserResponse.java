0421b1144e80ab8c2577da60897dfb593e514e93
tomoya0844 <dpon3285@yahoo.co.jp>
2023-11-20 12:28:56 +0900

    UserResponseのクラス追加

---
+++ src/main/java/com/kadai10/user/UserResponse.java
@@ -0,0 +1,15 @@
+package com.kadai10.user;
+
+
+public class UserResponse {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public UserResponse(String message) {
+        this.message = message;
+    }
+}
