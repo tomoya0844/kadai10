b0690fbfaf91ec293d52aa21c641490b3ab8ad79
tomoya0844 <146510558+tomoya0844@users.noreply.github.com>
2023-11-20 12:40:25 +0900

    Update UserResponse.java
    インデント処理

--- src/main/java/com/kadai10/user/UserResponse.java
+++ src/main/java/com/kadai10/user/UserResponse.java
@@ -1,14 +1,12 @@
 package com.kadai10.user;
 
-
 public class UserResponse {
-
     private String message;
 
     public String getMessage() {
         return message;
     }
-
+   
     public UserResponse(String message) {
         this.message = message;
     }
