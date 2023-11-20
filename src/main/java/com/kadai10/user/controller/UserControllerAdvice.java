ecae8dcadfdfc9cbab478a50439fa6e62233d270
tomoya0844 <146510558+tomoya0844@users.noreply.github.com>
2023-11-20 15:19:08 +0900

    修正

--- src/main/java/com/kadai10/user/controller/UserControllerAdvice.java
+++ src/main/java/com/kadai10/user/controller/UserControllerAdvice.java
@@ -6,22 +6,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
 import java.sql.SQLIntegrityConstraintViolationException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.ZonedDateTime;
 import java.util.Map;
 
 @ControllerAdvice
 public class UserControllerAdvice {
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException e, HttpServletRequest request) {
         Map<String, String> body = Map.of(
                 "timestamp", ZonedDateTime.now().toString(),
                 "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
                 "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
-                "message", "指定されたリソースが見つかりません:",
+                "message", "指定されたリソースが見つかりません",
                 "path", request.getRequestURI());
-    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -32,7 +33,7 @@ public class UserControllerAdvice {
                 "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 "message", "入力してください",
                 "path", request.getRequestURI());
-    return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
@@ -43,6 +44,6 @@ public class UserControllerAdvice {
                 "error", HttpStatus.CONFLICT.getReasonPhrase(),
                 "message", e.getMessage() + "データが既に存在しています。新しいデータを追加できません。",
                 "path", request.getRequestURI());
-    return new ResponseEntity<>(body, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(body, HttpStatus.CONFLICT);
     }
 }
