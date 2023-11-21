60a92d32f19c01c9702be2ec13e5a34012af92c8
tomoya0844 <dpon3285@yahoo.co.jp>
2023-11-21 19:44:27 +0900

    新しく作成 PATCHの処理の追加

---
+++ src/main/java/com/kadai10/user/UserUpdateRequest.java
@@ -0,0 +1,27 @@
+package com.kadai10.user;
+
+public class UserUpdateRequest {
+    String name;
+    String occupation;
+
+    public UserUpdateRequest(String name, String occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+}
