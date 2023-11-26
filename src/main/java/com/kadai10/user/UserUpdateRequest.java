04f0cabfbb807da429ad1eccf62d4e9fc1f8cbd0
tomoya0844 <146510558+tomoya0844@users.noreply.github.com>
2023-11-26 12:41:11 +0900

    Check styleの修正(UserUpdateRequest)
    インデント修正

--- src/main/java/com/kadai10/user/UserUpdateRequest.java
+++ src/main/java/com/kadai10/user/UserUpdateRequest.java
@@ -3,25 +3,21 @@ package com.kadai10.user;
 public class UserUpdateRequest {
   String name;
   String occupation;
-
+  
   public UserUpdateRequest(String name, String occupation) {
     this.name = name;
     this.occupation = occupation;
   }
-
   public void setName(String name) {
     this.name = name;
   }
-  
   public void setOccupation(String occupation) {
-  this.occupation = occupation;
-    }
-
+    this.occupation = occupation;
+  }
   public String getName() {
-  return name;
-    }
-
+    return name;
+  }
   public String getOccupation() {
-  return occupation;
-    }
+    return occupation;
+  }
 }
