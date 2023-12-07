40d4f983fb41711aa7e7358a37a3982b701ffe5b
tomoya0844 <dpon3285@yahoo.co.jp>
2023-12-08 00:16:30 +0900

    check styleに従い修正 GoogleChecksに設定して直した

--- src/main/java/com/kadai10/user/UserApplication.java
+++ src/main/java/com/kadai10/user/UserApplication.java
@@ -1,10 +1,12 @@
 package com.kadai10.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * ユーザーアプリケーションのエントリーポイントとなるクラスです. アプリケーション全体の初期化や実行フローの制御を担当します。
  */
+@SpringBootApplication
 public class UserApplication {
 
   /**
