9825fc36420d16c696be2efda6ca3cf772d47db2
tomoya0844 <dpon3285@yahoo.co.jp>
2023-12-31 02:53:34 +0900

    情報修正

--- src/test/resources/sqlannotation/insert-users.sql
+++ src/test/resources/sqlannotation/insert-users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (id, name, occupation)
-VALUES (1, "清⽔", "SE");
+VALUES (1, "小山", "警察官");
 INSERT INTO users (id, name, occupation)
-VALUES (2, "⼩⼭", "YouTuber");
+VALUES (2, "北野", "介護士");
 INSERT INTO users (id, name, occupation)
-VALUES (3, "⽥中", "看護師");
+VALUES (3, "田中", "看護師");
