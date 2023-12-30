e777f8c2814f467a1af5ac1e4376dee5a0a1ac1e
tomoya0844 <dpon3285@yahoo.co.jp>
2023-12-31 02:55:48 +0900

    Read処理のDBテスト 情報修正およびテストケース追加

--- src/test/java/com/kadai10/user/mapper/UserMapperTest.java
+++ src/test/java/com/kadai10/user/mapper/UserMapperTest.java
@@ -7,6 +7,7 @@ import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.kadai10.user.entity.User;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +30,42 @@ class UserMapperTest {
     assertThat(users)
         .hasSize(3)
         .contains(
-            new User(1, "清⽔", "SE"),
-            new User(2, "⼩⼭", "YouTuber"),
-            new User(3, "⽥中", "看護師"));
+            new User(1, "小山", "警察官"),
+            new User(2, "北野", "介護士"),
+            new User(3, "田中", "看護師"));
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  void 存在するユーザーのIDを指定した時に正常にユーザーが返されること() {
+    Optional<User> users = userMapper.findById(3);
+    assertThat(users)
+        .contains(new User(3, "田中", "看護師"));
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  void 存在しないユーザーのIDを指定した時に空の情報が返されること() {
+    Optional<User> users = userMapper.findById(39);
+    assertThat(users).isEmpty();
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  void 存在するユーザーの職業を指定した時に正常にユーザーが返されること() {
+    Optional<User> users = userMapper.findByOccupation("介護士");
+    assertThat(users)
+        .contains(new User(2, "北野", "介護士"));
+  }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  void 存在しないユーザーの職業を指定した時に空の情報が返されること() {
+    Optional<User> users = userMapper.findByOccupation("農家");
+    assertThat(users).isEmpty();
   }
 }
