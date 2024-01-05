5ef4de1ba81d0f57313164c4e2dd582f0f8b259a
tomoya0844 <dpon3285@yahoo.co.jp>
2024-01-05 23:15:18 +0900

    OccupationAlreadyExistsExceptionに名称変更

--- src/main/java/com/kadai10/user/excepention/OccupationAlreadyExistsException.java
+++ src/main/java/com/kadai10/user/excepention/OccupationAlreadyExistsException.java
@@ -4,14 +4,15 @@ package com.kadai10.user.excepention;
  * ユーザーが既に存在する場合にスローされる例外クラスです. ユーザーが特定の条件を満たさない場合や、IDに対応するユーザーが存在する場合に使用されます。
  */
 
-public class UserAlreadyExistsException extends RuntimeException {
+public class OccupationAlreadyExistsException extends RuntimeException {
 
   /**
    * ユーザーが既に存在する場合にスローされる例外クラス. この例外は、指定されたユーザーがデータベースやリポジトリで見つかった場合にスローされます
    *
    * @param message 例外メッセージ
    */
-  public UserAlreadyExistsException(final String message) {
+  public OccupationAlreadyExistsException(final String message) {
     super(message);
   }
 }
+
