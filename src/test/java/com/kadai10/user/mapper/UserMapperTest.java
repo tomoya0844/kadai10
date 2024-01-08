5c60e4defdca1e93ea252e9f93787dc24c57db6b
tomoya0844 <dpon3285@yahoo.co.jp>
2024-01-08 11:58:07 +0900

    Create処理のDBテスト

--- src/test/java/com/kadai10/user/mapper/UserMapperTest.java
+++ src/test/java/com/kadai10/user/mapper/UserMapperTest.java
@@ -68,4 +68,20 @@ class UserMapperTest {
     Optional<User> users = userMapper.findByOccupation("農家");
     assertThat(users).isEmpty();
   }
+
+  @Test
+  @DataSet(value = "datasets/users.yml")
+  @Transactional
+  public void 正常に新規のユーザーが登録できること() {
+    User user = new User(null, "山田", "建築士");
+    userMapper.insert(user);
+    List<User> users = userMapper.findAll();
+    assertThat(users).hasSize(4)
+        .contains(
+            new User(1, "小山", "警察官"),
+            new User(2, "北野", "介護士"),
+            new User(3, "田中", "看護師"),
+            user
+        );
+  }
 }
