06283e614a252bcade28b0671b96b1da4b46925f
tomoya0844 <dpon3285@yahoo.co.jp>
2024-01-09 23:23:57 +0900

    public recordに修正 デフォルトでgetterメソッドが生成されるため削除修正

--- src/main/java/com/kadai10/user/controller/request/UserRequest.java
+++ src/main/java/com/kadai10/user/controller/request/UserRequest.java
@@ -2,17 +2,12 @@ package com.kadai10.user.controller.request;
 
 /**
  * ユーザー情報のリクエストを表すクラスです. このクラスは新しいユーザーの作成や既存のユーザー情報の更新などの要求を受け取ります。
+ *
+ * @param name       ユーザーの名前を表すフィールド.
+ * @param occupation ユーザーの職業を表すフィールド.
  */
-public class UserRequest {
 
-  /**
-   * ユーザーの名前を表すフィールド.
-   */
-  private final String name;
-  /**
-   * ユーザーの職業を表すフィールド.
-   */
-  private final String occupation;
+public record UserRequest(String name, String occupation) {
 
   /**
    * UserRequest オブジェクトを生成するためのコンストラクタ.
@@ -20,26 +15,6 @@ public class UserRequest {
    * @param name       ユーザーの名前
    * @param occupation ユーザーの職業
    */
-  public UserRequest(final String name, final String occupation) {
-    this.name = name;
-    this.occupation = occupation;
-  }
-
-  /**
-   * ユーザーの名前を取得する.
-   *
-   * @return ユーザーの名前
-   */
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * ユーザーの職業を取得する.
-   *
-   * @return ユーザーの職業
-   */
-  public String getOccupation() {
-    return occupation;
+  public UserRequest {
   }
 }
