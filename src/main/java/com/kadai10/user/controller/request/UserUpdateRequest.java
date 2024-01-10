5160566df17f6717f32e47b3bf630ad3e855d5b8
tomoya0844 <dpon3285@yahoo.co.jp>
2024-01-10 23:09:46 +0900

    staticメソッド消去に伴う修正

--- src/main/java/com/kadai10/user/controller/request/UserUpdateRequest.java
+++ src/main/java/com/kadai10/user/controller/request/UserUpdateRequest.java
@@ -11,17 +11,15 @@ import lombok.Setter;
 public class UserUpdateRequest {
 
   /**
-   * 静的な名前を表すクラス変数. この変数はすべてのインスタンスで共有され、クラスから直接アクセス可能 -- GETTER -- ユーザーの名前を取得するメソッド. -- SETTER
-   * --ユーザーの名前を設定するメソッド.
-   */
-  @Getter
-  private static String name;
-  /**
    * 静的な職業を表すクラス変数. この変数はすべてのインスタンスで共有され、クラスから直接アクセス可能 -- GETTER -- ユーザーの職業を取得するメソッド. -- SETTER
    * --ユーザーの職業を設定するメソッド.
    */
-  @Getter
-  private static String occupation;
+  private String occupation;
+  /**
+   * 静的な名前を表すクラス変数. この変数はすべてのインスタンスで共有され、クラスから直接アクセス可能 -- GETTER -- ユーザーの名前を取得するメソッド. -- SETTER
+   * --ユーザーの名前を設定するメソッド.
+   */
+  private String name;
 
   /**
    * ユーザー情報を更新するためのリクエストオブジェクト.
@@ -30,25 +28,7 @@ public class UserUpdateRequest {
    * @param occupation ユーザーの新しい職業
    */
   public UserUpdateRequest(String name, String occupation) {
-    UserUpdateRequest.name = name;
-    UserUpdateRequest.occupation = occupation;
-  }
-
-  /**
-   * ユーザーの名前を設定するための静的メソッド.
-   *
-   * @param name ユーザーの新しい名前
-   */
-  public static void setName(String name) {
-    UserUpdateRequest.name = name;
-  }
-
-  /**
-   * ユーザーの職業を設定するための静的メソッド.
-   *
-   * @param occupation ユーザーの新しい職業
-   */
-  public static void setOccupation(String occupation) {
-    UserUpdateRequest.occupation = occupation;
+    this.name = name;
+    this.occupation = occupation;
   }
 }
